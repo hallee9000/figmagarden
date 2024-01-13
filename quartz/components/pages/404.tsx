da55db13e0e5a36047fe395fa87fb545f992cd3c
leadream <leadream4@gmail.com>
2024-01-13 14:31:12 +0800

    Quartz sync: Jan 13, 2024, 2:31 PM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -5,6 +5,7 @@ function NotFound() {
     <article class="popover-hint">
       <h1>404</h1>
       <p>Either this page is private or doesn't exist.</p>
+      <a href="/">Back to home</a>
     </article>
   )
 }
